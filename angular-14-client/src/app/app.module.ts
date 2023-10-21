b7d475457679dca24e4424ca043824b825960d68
quentin ROTOLO <quent.5.rotolo@gmail.com>
2023-10-21 22:58:57 +0200

    updating npm and firebase dependencies version

--- angular-14-client/src/app/app.module.ts
+++ angular-14-client/src/app/app.module.ts
@@ -3,6 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddDictionaryComponent } from './components/add-dictionary/add-dictionary.component'
@@ -14,7 +19,12 @@ import { DictionaryListComponent } from './components/dictionary-list/dictionary
     AppComponent,
     AddDictionaryComponent,
     DictionaryDetailsComponent,
-    DictionaryListComponent
+    DictionaryListComponent, 
+    // TODO
+    // AngularFireModule.initializeApp(environment.firebase),
+    // AngularFireDatabaseModule,
+    // AngularFirestoreModule,
+    // AngularFireStorageModule
   ],
   imports: [
     BrowserModule,
