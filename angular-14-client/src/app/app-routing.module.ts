9137f24c0c43caa934105a839685b5c955ad7ea7
quentin ROTOLO <quent.5.rotolo@gmail.com>
2023-11-27 19:04:43 +0100

    creating page-not-found component and adding the routing

--- angular-14-client/src/app/app-routing.module.ts
+++ angular-14-client/src/app/app-routing.module.ts
@@ -1,14 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DictionaryListComponent } from '../protected/dictionary-list/dictionary-list.component';
-import { DictionaryDetailsComponent } from '../protected/dictionary-details/dictionary-details.component';
-import { AddDictionaryComponent } from '../protected/add-dictionary/add-dictionary.component';
+import { PageNotFoundComponent } from 'src/core/components/page-not-found/page-not-found.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'dictionary', pathMatch: 'full' },
-  { path: 'dictionary', component: DictionaryListComponent },
-  { path: 'dictionary/:key', component: DictionaryDetailsComponent },
-  { path: 'add', component: AddDictionaryComponent }
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
