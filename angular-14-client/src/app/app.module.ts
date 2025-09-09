08cfe95c18a613e9300c5aecd3001798cf8018db
quentin ROTOLO <quent.5.rotolo@gmail.com>
2025-09-09 16:47:15 +0200

    temporary updates due to angular 19 migration

--- angular-14-client/src/app/app.module.ts
+++ angular-14-client/src/app/app.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { AngularFireModule } from '@angular/fire/compat';
-import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
-import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
-import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideDatabase, getDatabase } from '@angular/fire/database';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { provideStorage, getStorage } from '@angular/fire/storage';
 import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -25,10 +25,10 @@ import { DictionaryListComponent } from '../protected/dictionary-list/dictionary
     AppRoutingModule,
     FormsModule,
     HttpClientModule, 
-    AngularFireModule.initializeApp(environment.firebase),
-    AngularFireDatabaseModule,
-    AngularFirestoreModule,
-    AngularFireStorageModule
+  provideFirebaseApp(() => initializeApp(environment.firebase)),
+  provideFirestore(() => getFirestore()),
+  provideDatabase(() => getDatabase()),
+  provideStorage(() => getStorage())
   ],
   providers: [],
   bootstrap: [AppComponent]
